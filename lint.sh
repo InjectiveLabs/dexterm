3a1ce161448d9ad41fff66b7125f7c4fd42b788f
Dan Ξ <danandreist@gmail.com>
2020-02-28 16:41:05 +0200

    Delete lint.sh

--- lint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-# you should have golangci-lint installed:  https://github.com/golangci/golangci-lint
-# curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s v1.23.6
-golangci-lint run --no-config --issues-exit-code=1 --enable-all --disable=gocyclo --disable=nakedret --disable=gochecknoglobals --tests=false --disable=goimports --disable=wsl
